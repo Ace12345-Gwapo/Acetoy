c3bfde7efc5ac4d0cccfc88e3489effb9eda8568
Dave Luna <p.zhane132@gmail.com>
2024-12-17 11:39:04 +0700

    first commit

---
+++ Cplusplus.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    printf("this is my first time but this C is totally different from what ours\n\n");
+    printf("Enter your name please: ");
+    gets(name);
+    printf("Enter your age as well: ");
+    gets(age);
+    printf("Enter your current address please: ");
+    gets(add);
+    
+    return 0;
+}
---
+++ Secret.C
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+	puts("Kung gusto ko");
+	puts("\n\nGUSTO MO!!");
+	
+	return 0;
+}
